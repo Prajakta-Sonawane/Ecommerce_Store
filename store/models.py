5f8beab033bc5e476c9c2b5e83195884f741db2d
Prajakta Sonawane <prajakta.sonawane.in@gmail.com>
2021-08-09 09:21:09 +0530

    Add models.py

--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
+# return the product if is active
 class ProductManager(models.Manager):
     def get_queryset(self):
         return super(ProductManager, self).get_queryset().filter(is_active=True)
@@ -16,20 +17,20 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('store:category_list', args=[self.slug])
-    
+
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    Category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
     title = models.CharField(max_length=255)
-    painter = models.TextField(max_length=255, default='admin')
+    painter = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/')
     slug = models.SlugField(max_length=255)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -37,13 +38,12 @@ class Product(models.Model):
     objects = models.Manager()
     products = ProductManager()
 
-
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('-created',)
 
     def get_absolute_url(self):
-        return reverse('store:product_detail',args=[self.slug]) 
+        return reverse('store:product_detail', args=[self.slug])
 
     def __str__(self):
-        return self.title       
+        return self.title
